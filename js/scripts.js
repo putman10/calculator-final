6b7806b6b388ea29cf58efd37a637c112c0d7d78
Nikki Boyd and Ryan Putman <nikki@>
2018-05-31 14:38:49 -0700

    add radio buttons for all operations

--- js/scripts.js
+++ js/scripts.js
@@ -15,33 +15,22 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 // User interface (or front-end) logic:
-$(document).ready(function(){
-  $("form#add").submit(function(event){
+$(document).ready(function() {
+  $("form#calculator").submit(function() {
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
     var result = add(number1, number2);
-    $("#output").text(result);
-  });
-  $("form#sub").submit(function(event){
-    event.preventDefault();
-    var number1 = parseInt($("#sub1").val());
-    var number2 = parseInt($("#sub2").val());
-    var result = subtract(number1, number2);
-    $("#output").text(result);
-  });
-  $("form#multiply").submit(function(event){
-    event.preventDefault();
-    var number1 = parseInt($("#mult1").val());
-    var number2 = parseInt($("#mult2").val());
-    var result = multiply(number1, number2);
-    $("#output").text(result);
-  });
-  $("form#divide").submit(function(event){
-    event.preventDefault();
-    var number1 = parseInt($("#div1").val());
-    var number2 = parseInt($("#div2").val());
-    var result = divide(number1, number2);
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
     $("#output").text(result);
   });
 });
